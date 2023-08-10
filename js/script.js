48c87bc47e5097408560c94912bb6a382ba56939
Vladimir <stolyarow200138@gmail.com>
2023-08-10 18:47:19 +0500

    Фикс галереи

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let offset = 0;
 const sliderLine = document.querySelector('.gallery__photo');
 
 document.querySelector('.next').addEventListener('click', function() {
-    offset += 840;
-    if (offset > 2400) {
+    offset += 1000;
+    if (offset > 2000) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
@@ -11,9 +11,9 @@ document.querySelector('.next').addEventListener('click', function() {
 });
 
 document.querySelector('.prev').addEventListener('click', function() {
-    offset -= 840;
+    offset -= 1000;
     if (offset < 0) {
-        offset = 2400;
+        offset = 2000;
     }
     sliderLine.style.left = -offset + 'px';
 });
